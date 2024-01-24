e3a6249521d60db260b97c4e677cc9a061e885cd
“jliu937” <jliu937@gatech.edu>
2024-01-24 15:17:44 -0500

    Added Tutorial Images

--- src/help.ts
+++ src/help.ts
@@ -17,18 +17,28 @@ export class HelpPage extends LitElement {
       <!--<h1 class='h1'>How to Play</h1>-->
       <slot></slot>
       <div class="card">
-      <button @click=${this._onClickVideo} part="button" style = "position:relative; right:70px">
+       <img src="images//tutorial_image_1.png" alt="Tutorial 1" style="position:relative; right:20px; width:301.5px;height:267.75px;"> 
+       <img src="images//tutorial_image_2.png" alt="Tutorial 2" style="position:relative; width:301.5px;height:267.75px;"> 
+       <img src="images//tutorial_image_3.png" alt="Tutorial 3" style="position:relative; left:20px; width:301.5px;height:267.75px;">
+       <br> 
+       
+       <p> Select a Column! &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  
+        Block Opponent's Victory! &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+        Match 4 in a Row! </p>
+      </div>  
+      
+      <div class="card">
+        <p> Two players will alternate turns. <br>
+          Click on a column to drop you color disc into that column of the game board. <br>
+          The first player to match four in a row of their color discs is the winner. </p>
+      
+      <button @click=${this._onClickVideo} part="button" style = "position:relative; right:30px; height:120px; width:150px">
           Watch Video
       </button>
-      <button @click=${this._onClickTutorial} part="button" style = "position:relative; left:70px">
+      <button @click=${this._onClickTutorial} part="button" style = "position:relative; left:30px; height:120px; width:150px">
           Play Tutorial
       </button>
-      </div>  
-      <div class="card">
-        <p> Two players will alternate turns. Click on a column to drop you color disc into that column of the game board.
-          The first player to match four in a row of their color discs is the winner. </p>
-      </div>
-      
+
       <div class="card">
       <button @click=${this._onClickBack} part="button" style = "position:relative; left:250px; top:50px; height:75px; width:75px">
           Back
