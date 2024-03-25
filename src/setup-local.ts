6bda0f4f3aedd13ed237b7d409689e3750fa3f87
odile <odilevidrine@gatech.edu>
2024-03-25 16:02:29 -0400

    improved colors when clicking on randomize button, randomize button is now consistent across gamemodes

--- src/setup-local.ts
+++ src/setup-local.ts
@@ -17,22 +17,22 @@ export class SetupPage extends LitElement {
     return html`
       <slot></slot>
       <div class="card-deck">
-      <legend>Select Who Goes First</legend>
+      
       <div class="card">
           <input type="text" id = "p1_name" style = "position:relative; right:70px"  placeholder = "Player 1 Name" @change=${this.onChangeP1Name}/>
           <input type="text" id = "p2_name" style = "position:relative; left:70px"  placeholder = "Player 2 Name" @change=${this.onChangeP2Name}/>
         <br>
         <br>
+        <legend>Select Who Goes First</legend>
         <input type="radio" id = "p1" style = "position:relative; right:120px" value = "p1" name="firstPlayer"
             ?checked=${this.firstPlayer === 'p1'} @change=${this._onClickRadio}/>
             <label style = "position:relative; right:120px" for="p1"> Player 1 </label>
           <input type="radio" id = "p2" style = "position:relative; left:100px" value = "p2" name="firstPlayer"
             ?checked=${this.firstPlayer === 'p2'} @change=${this._onClickRadio} />
             <label style = "position:relative; left:100px" for= "p2"> Player 2 </label>
-          
         </div>
         <div class="card">
-          <button @click=${this.onClickRandom} part="button" style = "position:relative; height:55px; background-color: ${buttonColor.Green}">
+          <button @click=${this.onClickRandom} part="button" style = "position:relative; height:55px;" >
             Randomize
           </button>
         </div>
@@ -52,46 +52,10 @@ export class SetupPage extends LitElement {
     console.log(this.firstPlayer)
   }
 
-  private onClickRandom() {
-    // Randomly choose between 'p1' and 'p2' and set as firstPlayer
-    this.firstPlayer = Math.random() < 0.5 ? 'p1' : 'p2';
-  
-    // Force a refresh of the radio buttons by changing an attribute
-    this.refreshRadioButtons();
-  
-    
-    const buttonColor = this.randomize ? 'green' : 'red';
-    this.randomize = !this.randomize; // Toggle the randomize property
-    this.updateButtonColor(buttonColor);
-    
-   
-    this.requestUpdate();
-    console.log("First Player: " + this.firstPlayer)
-  }
-  
-  private refreshRadioButtons() {
-    
-    const p1Radio = this.shadowRoot?.querySelector('#p1') as HTMLInputElement;
-    const p2Radio = this.shadowRoot?.querySelector('#p2') as HTMLInputElement;
-  
-    if (p1Radio && p2Radio) {
-      
-      const refreshValue = (p1Radio.getAttribute('data-refresh') || 'false') === 'false' ? 'true' : 'false';
-      p1Radio.setAttribute('data-refresh', refreshValue);
-      p2Radio.setAttribute('data-refresh', refreshValue);
-  
-      
-      p1Radio.checked = this.firstPlayer === 'p1';
-      p2Radio.checked = this.firstPlayer === 'p2';
-    }
-  }
-  
-  private updateButtonColor(color: string) {
-    // Directly update the button color via shadow DOM, ensuring the element is an HTMLElement
-    const button = this.shadowRoot?.querySelector('button[part="button"]') as HTMLElement | null;
-    if (button) {
-      button.style.backgroundColor = color;
-    }
+  private onClickRandom(e: { target: { style: string; }; }) {
+    this.randomize = !this.randomize
+    e.target.style = this.randomize ? "position:relative; height:55px; color:black; background-color:"+buttonColor.Orange : "position:relative; height:55px;"
+    console.log("randomize: " + this.randomize)
   }
 
   private onChangeP1Name(e: {target: { value: string; }}){
